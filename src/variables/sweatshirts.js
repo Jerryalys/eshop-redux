4d5ec2119129b1aa2ba806f69cc1728bde560116
Jungleyes1980 <gerasimosalisandratos@hotmail.com>
2021-10-18 20:29:23 +0300

    Quantity Add

--- src/variables/sweatshirts.js
+++ src/variables/sweatshirts.js
@@ -5,7 +5,8 @@ const sweatshirts = [
       description: ' Loose-Fitting Hoodie With Adjustable Drawstring Hood',
       price: '35$',
       image: '../assets/img/sweatshirts/sweatshirts1.jpg,',
-      code: '335'
+      code: '335',
+      quantity: 1
     },
     {
       id: 2,
@@ -13,7 +14,8 @@ const sweatshirts = [
       description: 'Loose-Fitting Sweatshirt With a Round Neckline',
       price: '30$',
       image: '../assets/img/sweatshirts/sweatshirts2.jpg',
-      code: '330'
+      code: '330',
+      quantity: 1
     },
     {
       id: 3,
@@ -21,7 +23,8 @@ const sweatshirts = [
       description: 'Loose-Fitting Hoodie with an Adjustable Hood and Long Sleeves',
       price: '25$',
       image: '../assets/img/sweatshirts/sweatshirts3.jpg',
-      code: '325'
+      code: '325',
+      quantity: 1
     },
     {
       id: 4,
@@ -29,7 +32,8 @@ const sweatshirts = [
       description: 'Vest Made of Stretchy Cotton Round Neckline Ribbed Trims',
       price: '35$',
       image: '../assets/img/sweatshirts/sweatshirts4.jpg',
-      code: '335'
+      code: '335',
+      quantity: 1
     },
     {
       id: 5,
@@ -37,7 +41,8 @@ const sweatshirts = [
       description: 'Loose-Fitting Made of Cotton Open Neckline and Long Sleeves',
       price: '45$',
       image: '../assets/img/sweatshirts/sweatshirts5.jpg',
-      code: '345'
+      code: '345',
+      quantity: 1
     },
     {
       id: 6,
@@ -45,7 +50,8 @@ const sweatshirts = [
       description: 'Loose-Fitting Hoodie With an Adjustable Hood and Long Sleeves',
       price: '40$',
       image: '../assets/img/sweatshirts/sweatshirts6.jpg',
-      code: '340'
+      code: '340',
+      quantity: 1
     },
   ];
 
