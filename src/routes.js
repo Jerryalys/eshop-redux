a08cb9c23eb24f457394ca32fbbc69de91c45903
Jungleyes1980 <gerasimosalisandratos@hotmail.com>
2021-10-18 20:23:20 +0300

    Updated Route

--- src/routes.js
+++ src/routes.js
@@ -73,27 +73,5 @@ var dashRoutes = [
     component: Sweatshirts,
     layout: "/admin",
   },
-  // {
-  //   path: "/sweatshirts",
-  //   name: "Sweatshirts",
-  //   icon: "shopping_box",
-  //   component: Sweatshirts,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/typography",
-  //   name: "Typography",
-  //   icon: "design-2_ruler-pencil",
-  //   component: Typography,
-  //   layout: "/admin",
-  // },
-  // {
-  //   pro: true,
-  //   path: "/upgrade",
-  //   name: "Upgrade to PRO",
-  //   icon: "objects_spaceship",
-  //   component: Upgrade,
-  //   layout: "/admin",
-  // },
 ];
 export default dashRoutes;
