2218c58b1518aa702e1d34ef178afacdb9d26009
Jungleyes1980 <gerasimosalisandratos@hotmail.com>
2021-10-22 17:42:00 +0300

    Jackets Creation

---
+++ src/variables/jackets.js
@@ -0,0 +1,59 @@
+const jackets = [
+    {
+      id: 1,
+      title: 'Wool Blend Jacket',
+      description: 'Collared Wool Blend Jacket. Buttoned Cuffs. Button-Up Front',
+      price: '150$',
+      image: '../assets/img/jackets/jackets1.jpg',
+      code: '450',
+      quantity: 1
+    },
+    {
+
+      id: 2,
+      title: 'Water-Repellent Quilted Jacket',
+      description: 'Jacket With a High Collar And Long Sleeves With Elasticated Cuffs',
+      price: '80$',
+      image: '../assets/img/jackets/jackets2.jpg',
+      code: '460',
+      quantity: 1
+    },
+    {
+      id: 3,
+      title: 'Puffer Jacket With Pockets',
+      description: 'Puffer Jacket With Long Sleeves And Adjustable Tabs On The Cuffs',
+      price: '80$',
+      image: '../assets/img/jackets/jackets3.jpg',
+      code: '480',
+      quantity: 1
+    },
+    {
+      id: 4,
+      title: 'Basic Quilted Jacket',
+      description: 'Puffer Jacket With A High Neck And Long Sleeves With Elastic Cuffs',
+      price: '60$',
+      image: '../assets/img/jackets/jackets4.jpg',
+      code: '460',
+      quantity: 1,
+    },
+    {
+      id: 5,
+      title: 'Contrast Corduroy Jacket',
+      description: 'Collared Jacket With Long Sleeves, Chest Flap And Hip Welt Pockets',
+      price: '70$',
+      image: '../assets/img/jackets/jackets5.jpg',
+      code: '470',
+      quantity: 1
+    },
+    {
+      id: 6,
+      title: 'Animal Print Jacket',
+      description: 'Jacket With Contrast Collar And Double Welt Pockets At The Hip',
+      price: '75$',
+      image: '../assets/img/jackets/jackets6.jpg',
+      code: '475',
+      quantity: 1
+    },
+  ];
+
+  export default jackets;
