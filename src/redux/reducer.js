b3e29b14a6ec60cedf8a357dbcfdebc7d91b00f2
Jungleyes1980 <gerasimosalisandratos@hotmail.com>
2021-10-27 11:16:24 +0300

    Add filter method

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,68 +1,34 @@
-import { ADD_TO_CART } from './constants';
-import { REMOVE_FROM_CART } from './constants';
-import { ADD_QUANTITY} from './constants';
-import { SUB_QUANTITY}  from './constants';
-import { EMPTY_CART}  from './constants';
+import { ADD_TO_CART } from './constants'
+import { REMOVE_FROM_CART } from './constants'
+// import { ADD_QUANTITY} from './constants'
+// import { SUB_QUANTITY}  from './constants'
+// import { EMPTY_CART}  from './constants'
 
 
 const initialState = {
     cart: [],
-  };
+  }
 const ShoppinReducer = (state = initialState, action) => {
-  
   switch (action.type) {
-    
     case ADD_TO_CART:
-      console.log('action: ', action);
       const newCart = [...state.cart]
       
-  newCart.push(action.payload)
-  console.log('newCart: ', newCart);
-    return {
+      newCart.push(action.payload)
+      return {
         ...state,
         cart: newCart
-    };
+      }
+
     case REMOVE_FROM_CART:
-    return {
+      const cart = [...state.cart]
+      const updatedCart = cart.filter(item => item.id !== action.payload.id)
+      return {
         ...state,
-        products: state.products.map(product =>
-        product.id === action.id
-            ? {...product, selected: false, quantity: 1}
-            : product,
-        ),
-    };
-    case ADD_QUANTITY:
-    return {
-        ...state,
-        products: state.products.map(product =>
-        product.id === action.id
-            ? {...product, quantity: product.quantity + 1}
-            : product,
-        ),
-    };
-    case SUB_QUANTITY:
-    return {
-        ...state,
-        products: state.products.map(product =>
-        product.id === action.id
-            ? {
-                ...product,
-                quantity: product.quantity !== 1 ? product.quantity - 1 : 1,
-            }
-            : product,
-        ),
-    };
-    case EMPTY_CART:
-    return {
-        ...state,
-        products: state.products.map(product =>
-        product.selected
-            ? {...product, selected: false, quantity: 1}
-            : product,
-        ),
-    };
+        cart: updatedCart
+      }
+    
     default:
     return state
   }
 }
-  export default ShoppinReducer;
+  export default ShoppinReducer
